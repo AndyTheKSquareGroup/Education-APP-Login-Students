e398891f956eddbc5e814b296fbf9899a68128af
Andy Ku <andy.ku@theksquaregroup.com>
2021-10-03 22:40:44 -0500

    first version

---
+++ lib/screens/studentInfoEvents.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SchoolEventsEstudents extends StatelessWidget {
+  String titleEvent = "";
+  String content = "";
+
+  SchoolEventsEstudents({
+    required this.titleEvent,
+    required this.content,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("$titleEvent"),
+            Text("$content"),
+          ],
+        ),
+      ),
+    );
+  }
+}
